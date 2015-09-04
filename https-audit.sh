453415f38814e652064a5c3c9e9f43cb7339e299
Zachsis <zachary.hatsis@gmail.com>
2015-09-04 10:36:21 -0600

    updated stuff

--- https-audit.sh
+++ https-audit.sh
@@ -1,11 +1,24 @@
 #!/bin/bash
+echo "what port to scan?"
+read PORT
+echo "what network to scan?"
+read NETWORK
 
-for ip in $(cat iplist.txt)
+nmap -p${PORT} ${NETWORK} --open -oG nmaptemp
+
+OUTFILE=$(date +%F-%H.%M)
+
+echo "[+] testing all port 80 hosts"
+for ip in $( awk -F" " '{print $2}' < nmaptemp )
 do
-        CODE=$(timeout 10 curl -s -o /dev/null -w "%{http_code}" $ip)
+        CODE=$(timeout 10 curl -s -o /dev/null -w "%{http_code}" http://$ip)
         if [[ "$CODE" = "301" ]] || [[ "$CODE" = "302" ]]; then
-                echo "Status code is $CODE Redirection DETECTED!"
+                echo "$CODE,Redirection DETECTED, $ip" >> ${OUTFILE}
         else
-                echo "Status code is $CODE No redirection.."
+                echo "$CODE,No redirect, $ip" >> ${OUTFILE}
         fi
 done
+
+rm nmaptemp
+
+echo '[+] done'
